5fd805aa4934f7242152bfd6ed1148e875eca86b
Ayush ghosh <ghoshayush910@gmail.com>
2025-02-14 02:03:56 +0530

    deh liya kaisa kam karte hai Any, unknown, void, nulll, undefined, Never

--- Any_unknown_etc/app.ts
+++ Any_unknown_etc/app.ts
@@ -1,6 +1,43 @@
-// Any, Unknown < void, Null, undefined,  Never
+// Any, Unknown , void, Null, undefined,  Never
 
 let num1: number; //number Type
 a = 12;
 // a = true;
 
+let ayush = 12; //Any type
+// ayush = "harsh";
+
+ayush.toExponential
+
+
+// unknown type
+let gaurango: unknown;
+gaurango = 12;
+gaurango = "ghosh";
+
+if(typeof gaurango === "string"){
+    a.toString
+}
+
+// void, int different return type
+function khana() : void{
+    console.log("mera type void hai")
+}
+
+function khanaPina() : Number{
+    return 12;
+}
+
+// null type use
+let ana: string | null;
+// ana = 12;
+
+// undefined decleration
+let amwara:undefined;
+
+// Never Type
+function continus(): never {
+    while (true){}
+}
+
+console.log("next part of code please")
