789a61c4b2d0a9755e0de4a9184736cf62d2420d
Ayush ghosh <ghoshayush910@gmail.com>
2025-02-14 02:35:48 +0530

    Type_interfaces & aliases

---
+++ Iterfaces & Type Aliases/practice.ts
@@ -0,0 +1,20 @@
+// Interface and Type Aliases
+
+function hamaraPyar(alu: number, tomato: string){
+    alu.toLocaleString
+    tomato.charCodeAt
+}
+
+// interface hai kisi bhi object ka sakal bola jata hai
+interface user{
+    name: string,
+    email: string,
+    password: string,
+    gender?: string
+}
+function getDataOfUser(obj: user){
+    obj.name,
+    obj.password
+}
+
+getDataOfUser({name: "ayush", email: "ghoshayush3084@gmail.com", password: "Ayu@30987"})
