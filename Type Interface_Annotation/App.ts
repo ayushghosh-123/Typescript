a763df2a4efda1b90c4a058d07e1c7f537f9ff1c
Ayush ghosh <ghoshayush910@gmail.com>
2025-02-14 02:11:40 +0530

    Basic understanding TYPE INTERFERENCE AND TYPE aNNOTATION

---
+++ Type Interface_Annotation/App.ts
@@ -0,0 +1,12 @@
+// learn Type inference and type annotation
+
+// Type inference
+let ayush1 = "12";
+
+// automaticaally defined the type
+
+// Type Annotation
+let ayush2 : number | boolean | string;
+ayush2 = 12;
+ayush2 = true;
+ayush2 = "i love you";
