eec3e63a808ab5eb0b7f314a2af6eb14b8319f97
Ayush ghosh <ghoshayush910@gmail.com>
2025-02-14 01:44:29 +0530

    i learn enum at few time ago

---
+++ Any_unknown_etc/app.ts
@@ -0,0 +1,6 @@
+// Any, Unknown < void, Null, undefined,  Never
+
+let num1: number; //number Type
+a = 12;
+// a = true;
+
---
+++ Enums/app.js
@@ -0,0 +1,17 @@
+// enumerations
+var Userrole;
+(function (Userrole) {
+    Userrole["Admin"] = "admin";
+    Userrole["Guest"] = "guest";
+    Userrole["Super_admin"] = "super_admin";
+})(Userrole || (Userrole = {}));
+
+Userrole.Super_admin;
+
+var Statuscode;
+(function (Statuscode) {
+    Statuscode["Abandent"] = "abadent status code 500";
+    Statuscode["Notfound"] = "nt found status code 404";
+})(Statuscode || (Statuscode = {}));
+
+Statuscode.Notfound;
