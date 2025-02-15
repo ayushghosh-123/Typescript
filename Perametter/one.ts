3bf4eb1bc3e7f49912bd5e578fd9c10b29cbee14
Ayush ghosh <ghoshayush910@gmail.com>
2025-02-15 12:46:45 +0530

    perametter properties parlia meri jann

---
+++ Perametter/one.ts
@@ -0,0 +1,10 @@
+// pramater property
+
+class GOAT{
+    constructor(public name: string, public age: number, public geneder?: string){
+
+    }
+}
+
+let user1 = new GOAT("Ayush", 20, "Male")
+let user2 = new GOAT("Gaurango", 26)
